e5d185be6f9c7a6beca2ca13040ded220347ba1d
TheStarRover <userandr222@gmail.com>
2025-09-30 15:40:44 +0400

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Знаходимо список з id="categories"
+const categoriesList = document.querySelector("#categories");
+
+// Перебираємо кожен елемент інгридиєнтів
+ingredients.forEach((ingredient) => {
+  const newLi = document.createElement("li");
+
+  const newContent = document.createTextNode(ingredient);
+
+  newLi.appendChild(newContent);
+
+  newLi.appendChild("class=item");
+
+  categoriesList.appendChild(newLi);
+});
+
+// document.querySelectorAll("li");
