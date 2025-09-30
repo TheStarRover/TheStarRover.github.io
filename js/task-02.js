274afb90aa85df32ac0e521bbdc500ff53a0ab89
TheStarRover <userandr222@gmail.com>
2025-09-30 15:40:49 +0400

    task2 update

--- js/task-02.js
+++ js/task-02.js
@@ -2,25 +2,21 @@ const ingredients = [
   "Potatoes",
   "Mushrooms",
   "Garlic",
-  "Tomatos",
+  "Tomatoes",
   "Herbs",
   "Condiments",
 ];
 
-// Знаходимо список з id="categories"
-const categoriesList = document.querySelector("#categories");
-
-// Перебираємо кожен елемент інгридиєнтів
-ingredients.forEach((ingredient) => {
-  const newLi = document.createElement("li");
-
-  const newContent = document.createTextNode(ingredient);
-
-  newLi.appendChild(newContent);
-
-  newLi.appendChild("class=item");
-
-  categoriesList.appendChild(newLi);
+// Створюємо масив <li> елементів
+const items = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
 });
 
-// document.querySelectorAll("li");
+// Знаходимо список <ul> з id="ingredients"
+const ingredientsList = document.querySelector("#ingredients");
+
+// Додаємо всі елементи за одну операцію
+ingredientsList.append(...items);
